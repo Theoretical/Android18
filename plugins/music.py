08e90747573de80b0821f7aea1e3d33f8717be5c
Theoretical <knidalee@gmail.com>
2017-01-12 19:04:01 -0500

    a

--- plugins/music.py
+++ plugins/music.py
@@ -1,1 +1,9 @@
 def initialize(zulia): pass
+
+async def on_message(zulia, msg, msg_obj):
+    callback_func = 'on_' + msg[0]
+
+    if hasattr(modules[__name__], callback_func):
+        await getattr(modules[__name__], callback_func)(zulia, msg, msg_obj)
+        return True
+    return False
